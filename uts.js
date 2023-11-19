d5abfefe78759a99632a32eb30a2dc9b44f42659
Qonita09 <144678576+Qonita09@users.noreply.github.com>
2023-11-19 18:20:02 +0700

    Add files via upload

---
+++ uts.js
@@ -0,0 +1,13 @@
+//pemberitahuan 
+alert('Selamat Datang di Website Kami!')
+
+// sidebar
+const menu = document.querySelector('.navbar #menu');
+const nav = document.querySelector('.navbar .navbar-nav');
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('active')
+});
+
+
+
